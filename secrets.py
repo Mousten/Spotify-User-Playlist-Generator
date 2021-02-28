41e09250a6d4c36e0565c5daab3deb5ccf287fcd
Njoro Kuria <njoro.kuria@gmail.com>
2021-02-28 19:29:04 +0300

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,5 @@
+# After creating an app on Spotify developers dashboard, insert the following details below
+USER_CLIENT_ID = "Insert your client ID here"
+USER_CLIENT_SECRET = "Insert your Client Secret here"
+USER_REDIRECT_URI = "https://httpbin.org/anything" # you can use any Redirect URI you like, I prefer httpbin as it Outputs Input
+spotify_user_id = "Insert your User Id here"
